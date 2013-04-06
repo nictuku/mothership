09a8924217818d8528cc8a416efe9ef0eb371611
Yves Junqueira <yves.junqueira@gmail.com>
2013-04-07 00:59:53 +0200

    The mothership receives heartbeats from agents and shows them in a web page.

---
+++ mothership/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var (
+	port = flag.Int("port", 8080, "Port on which to run the web server.")
+)
+
+const (
+	username = "nictuku" // XXX
+)
+
+const indexTemplate = `
+<html>
+<head><title>Mothership</title>
+</head>
+<body>
+	<h1>Logged in as Yves Junqueira</h1>
+	<h2>List of servers</h2>
+	<table>
+	<th>hostname</th><th>IP</th><th>Last seen</th>
+	{{range .}}
+	<tr><td><a href="ssh://{{ if .Username }}{{.Username}}@{{ end }}{{.IP}}:{{.SSHPort}}">{{.Hostname}}</a></td><td>{{.IP}}</td><td>{{.LastContact}}</td></tr>
+	{{end}}
+	</table>
+</body>
+</html>
+
+`
+
+var index *template.Template
+
+type ServerInfo struct {
+	Hostname    string
+	Username    string
+	SSHPort     int
+	IP          string
+	LastContact time.Time
+}
+
+// XXX lock.
+type serversInfo struct {
+	sync.RWMutex
+	Info map[string]ServerInfo
+}
+
+var servers *serversInfo
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	servers.Lock()
+	defer servers.Unlock()
+	hostname := req.FormValue("hostname")
+	if hostname != "" {
+		s := ServerInfo{
+			Hostname:    hostname,
+			Username:    username,
+			LastContact: time.Now(),
+		}
+		sshPort, err := strconv.Atoi(req.FormValue("sshPort"))
+		if sshPort == 0 || err != nil {
+			sshPort = -1
+		}
+		s.SSHPort = sshPort
+
+		if ip, _, err := net.SplitHostPort(req.RemoteAddr); err == nil && ip != "" {
+			s.IP = ip
+		}
+
+		servers.Info[s.Hostname] = s
+		log.Println("updated server", s.Hostname)
+		io.WriteString(w, "ok")
+		return
+	}
+
+	err := index.Execute(w, servers.Info)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	flag.Parse()
+	index = template.Must(template.New("index").Parse(indexTemplate))
+	servers = &serversInfo{Info: map[string]ServerInfo{}}
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+}
