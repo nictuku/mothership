736af1e752ca5b48860f486991928c7489675342
Yves Junqueira <yves.junqueira@gmail.com>
2013-04-07 02:49:22 +0200

    don't expand variables inside the heredoc.

--- agent/install.sh
+++ agent/install.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/myservers
 cd $HOME/myservers
 curl -sLO http://geekroot.com/static/agent
 chmod 700 agent
-cat <<EOF> run.sh
+cat <<"EOF"> run.sh
 nohup ./agent 2> /dev/null &
 echo $! > pid
 EOF
