62a9bffb5150414433556c7a5d6858c9e4e19ce1
Yves Junqueira <yves.junqueira@gmail.com>
2014-04-22 02:02:18 +0200

    Config, part 1

---
+++ mothership/config.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+func makeConfDir() string {
+	dir := "/var/run/mothership"
+	env := os.Environ()
+	for _, e := range env {
+		if strings.HasPrefix(e, "HOME=") {
+			dir = strings.SplitN(e, "=", 2)[1]
+			dir = path.Join(dir, ".mothership")
+		}
+	}
+	// Ignore errors.
+	os.MkdirAll(dir, 0750)
+
+	if s, err := os.Stat(dir); err != nil {
+		log.Fatal("stat config dir", err)
+	} else if !s.IsDir() {
+		log.Fatalf("Dir %v expected directory, got %v", dir, s)
+	}
+	return dir
+}
+
+func confPath() string {
+	dir := makeConfDir()
+	return path.Join(dir, "mothership.json")
+}
+
+type Config struct {
+	PushoverKey         string
+	PushoverDestination string
+}
+
+// readConfig reads the mothership configuration from $HOME/.mothership/mothership.json and returns
+// the parsed Config.
+func readConf() (cfg Config, err error) {
+	file, err := os.Open(confPath())
+	if err != nil {
+		return cfg, err
+	}
+	decoder := json.NewDecoder(file)
+	return cfg, decoder.Decode(&cfg)
+}
