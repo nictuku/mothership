58f7e54ec4319068616bb073693bc13e617a081f
Yves Junqueira <yves.junqueira@gmail.com>
2014-04-27 22:55:12 +0200

    Create an empty file that will be used to manage hosts

---
+++ mothership/hosts.go
@@ -0,0 +1,7 @@
+package main
+
+// How updates should work:
+// - starting the agent for the first time will make it generate a new shared key, stores locally
+// and inform it to the server when registering.
+// - run "zmon register <email@>" to assign it to a user.
+// -- alternative (more secure): "zmon register" and then follow the link it prints.
